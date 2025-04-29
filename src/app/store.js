aac977106b8045099220de68f6187da4725ada7e
Hebron <jsfast2@gmail.com>
2025-04-29 20:54:03 +0700

    update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
-import usernameReducer from '../features/login/usernameSlice'
 import productId from '../features/produk/productSlicer'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    username: usernameReducer,
     product: productId
   },
 })
